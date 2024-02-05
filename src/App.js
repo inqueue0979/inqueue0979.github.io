92293bb5bd8f53edf3765e49b955e6ee78263c83
Wonjae Jo <inqueue0979@gmail.com>
2024-02-06 02:56:10 +0900

    Added SUITE Fonts

--- src/App.js
+++ src/App.js
@@ -6,21 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <h2 className=' text-blue-700 text-2xl font-bold'>Hello, React!</h2>
-        </div>
+        <h1 className='font-extrabold'>{'>'} jowonjae_ {'<'}</h1>
       </header>
     </div>
   );
