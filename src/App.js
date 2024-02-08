8bf33077998eede34edd58124316b4dd6f753005
Wonjae Jo <inqueue0979@gmail.com>
2024-02-09 00:06:38 +0900

    Initial React & Tailwind Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import './output.css'
 
+function Header(){
+  return (
+      <header className="AppBar">
+        
+      </header>
+  )
+}
+
+function Content1()
+{
+  return (
+    <article className="App-header">
+      <div className='text-center w-1/3 md:w-1/4'>
+          <img className='w-full' alt='brick' src='img/rulernBrush.png'></img>
+      </div>
+      <div className='font-black text-2xl text-center tracking-tight md:text-4xl'>
+        <h1>Keep On Studying for Society!</h1>
+        <h1>소프트웨어융합대학 학술동아리 KOSS</h1>
+      </div>
+    </article>
+)
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className='font-extrabold'>{'>'} jowonjae_ {'<'}</h1>
-      </header>
+    <div>
+      <Header></Header>
+      <Content1></Content1>
     </div>
   );
 }
